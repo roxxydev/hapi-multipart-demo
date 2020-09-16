7f3a474b24a872eab6f5849ed5d26ecbaa84733e
Roxxy Rafael <roxxydev@gmail.com>
2020-09-16 13:31:44 +0800

    add downloading of uploaded file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const Fs = require('fs');
 const Hapi = require('@hapi/hapi');
+const Fs = require('fs');
 const Stream = require('stream');
+const Path = require('path');
+const Mime = require('mime');
 
 const init = async () => {
   const server = Hapi.server({
@@ -36,6 +38,34 @@ const init = async () => {
         allow: 'multipart/form-data',
         maxBytes: 25 * 1048576
       }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/download',
+    handler: (request, h) => {
+      const files = Fs.readdirSync(`${__dirname}/uploads/`);
+      const filename = files && files.length > 0 ? files[0] : '';
+
+      if (!filename) {
+        return h.response(404);
+      }
+
+      const fileExt = Path.extname(filename).replace(/\./g, '');
+      const mimeType = Mime.getType(fileExt);
+
+      const outputFilePath = `${__dirname}/uploads/${filename}`;
+      const fileBuffer = Fs.readFileSync(outputFilePath);
+
+      const response = h.response(fileBuffer);
+      response.code(200);
+      response.bytes(fileBuffer.length);
+      response.type('text/plain');
+      response.header('Content-Type', mimeType);
+      response.header('Content-Disposition', `attachment; filename="${filename}"`);
+
+      return response;
     }
   });
 
